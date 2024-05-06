f941eaf79e6557e02bb61d60af2954ad36545bf4
gaurav chauhan <gaurav.chauhan@innowatts.com>
2024-05-06 16:47:31 +0530

    adding new stuff

--- assets/header.js
+++ assets/header.js
@@ -4,11 +4,25 @@ let items = document.querySelectorAll(".header__inline-menu")[1].querySelectorAl
 items.forEach(item => {
     item.addEventListener("mouseover", () => {
         item.setAttribute("open", true);
+
+        let dropdownItems = item.querySelectorAll("li");
+        dropdownItems.forEach(item => {
+            item.addEventListener("mouseover", () => {
+                item.classList.add("menu-item-gc-dropdown");
+            });
+            item.addEventListener("mouseleave", () => {
+                item.classList.remove("menu-item-gc-dropdown");
+            });
+        })
+
         item.querySelector("ul").addEventListener("mouseleave", () => {
             item.removeAttribute("open");
         });
         item.addEventListener("mouseleave", () => {
             item.removeAttribute("open");
+            dropdownItems.forEach(item => {
+                item.classList.remove("menu-item-gc-dropdown");
+            })
         });
     });
 
